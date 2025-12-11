c2459054995f693058d5060dee56483626fc7ef8
Daniyal Arshaq S. <arshaqdaniyal@gmail.com>
2025-12-11 12:39:00 +0700

    [FIX] Perbaikan di main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,49 +14,48 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            Hello, I&apos;m Daniyal.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
+            I&apos;m a passionate Software Engineer with a strong background in
+            web development and mobile development, and a deep love for creating
+            memorable and fun experiences. With a keen eye in UI/UX and graphic
+            design, I bring ideas to life through code. Let&apos;s connect and
+            explore how we can create something amazing together.
+          </p>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Ready to start a project? Let&apos;s get started!{" "}
             <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              href="/contact/page.tsx"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-              Templates
+              Contact me
             </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="/contact/page.tsx"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
+              // className="dark:invert"
+              src="/img/icons/arrow-up-right-01.svg"
+              alt="Arrow Icon"
               width={16}
               height={16}
             />
-            Deploy Now
+            Contact Me
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://github.com/Archaniels/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            GitHub
           </a>
         </div>
       </main>
